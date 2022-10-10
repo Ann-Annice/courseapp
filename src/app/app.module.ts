381f516d0ad095cdd94850c2c440f4c106740a45
Ann-Annice <annannice98@gmail.com>
2022-10-10 16:00:11 +0530

    course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CourseappComponent } from './courseapp/courseapp.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewallcoursesComponent } from './viewallcourses/viewallcourses.component';
+const appRoutes:Routes=[
+  {
+    path:"",component:CourseappComponent
+  },
+  {
+    path:"view",component:ViewallcoursesComponent
+  },
+ 
+ 
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CourseappComponent,
+    ViewallcoursesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/viewallcourses/viewallcourses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewallcourses',
+  templateUrl: './viewallcourses.component.html',
+  styleUrls: ['./viewallcourses.component.css']
+})
+export class ViewallcoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
